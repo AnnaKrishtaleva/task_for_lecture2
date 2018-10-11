a0bf5cbe6ec0d4dc41dc13c8f840a207c863bd77
Anna <crishtaleva@mail.ru>
2018-10-12 00:38:09 +0300

    Выполнено задание ко второй лабораторной

---
+++ test1/main.cpp
@@ -0,0 +1,122 @@
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+#include <cilk/reducer_max.h>
+#include <cilk/reducer_min.h>
+#include <cilk/reducer_vector.h>
+#include <vector>
+#include <chrono>
+
+using namespace std::chrono;
+
+/// функция ReducerMaxTest() определяет максимальный элемент массива,
+/// переданного ей в качестве аргумента, и его позицию
+/// mass_pointer - указатель исходный массив целых чисел
+/// size - количество элементов в массиве
+void ReducerMaxTest(int *mass_pointer, const long size)
+{
+	cilk::reducer<cilk::op_max_index<long, int>> maximum;
+	cilk_for(long i = 0; i < size; ++i)
+	{
+		maximum->calc_max(i, mass_pointer[i]);
+	}
+	printf("Maximal element = %d has index = %d\n\n",
+		maximum->get_reference(), maximum->get_index_reference());
+}
+
+
+/// функция ReducerMinTest() определяет мминимальный элемент массива,
+/// переданного ей в качестве аргумента, и его позицию
+/// mass_pointer - указатель исходный массив целых чисел
+/// size - количество элементов в массиве
+void ReducerMinTest(int *mass_pointer, const long size)
+{
+	cilk::reducer<cilk::op_min_index<long, int>> minimum;
+	cilk_for(long i = 0; i < size; ++i)
+	{
+		minimum->calc_min(i, mass_pointer[i]);
+	}
+	printf("Minimum element = %d has index = %d\n\n",
+		minimum->get_reference(), minimum->get_index_reference());
+}
+
+/// функция ParallelSort() сортирует массив в порядке возрастани¤
+/// begin - указатель на первый элемент исходного массива
+/// end - указатель на последний элемент исходного массива
+void ParallelSort(int *begin, int *end)
+{
+	if (begin != end)
+	{
+		--end;
+		int *middle = std::partition(begin, end, std::bind2nd(std::less<int>(), *end));
+		std::swap(*end, *middle);
+		cilk_spawn ParallelSort(begin, middle);
+		ParallelSort(++middle, ++end);
+		cilk_sync;
+	}
+}
+/// функция CompareForAndCilk_For() выводить на консоль время работы стандартного цикла for и параллельного цикла cilk_for от Intel Cilk Plus
+/// sz - размер массива
+void CompareForAndCilk_For(size_t sz) 
+{
+	std::vector<int> vec;
+	cilk::reducer<cilk::op_vector<int>> red_vec;
+
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	for (int i = 0; i < sz; ++i)
+	{
+		vec.push_back(rand() % 20000 + 1);
+	}
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	duration<double> duration = (t2 - t1);
+	printf("Standard duration = %f ticks\n\n", duration.count());
+
+	t1 = high_resolution_clock::now();
+	cilk_for(int i = 0; i < sz; ++i)
+	{
+		red_vec->push_back(rand() % 20000 + 1);
+	}
+	t2 = high_resolution_clock::now();
+	duration = (t2 - t1);
+	printf("Cilk duration = %f ticks\n\n", duration.count());
+}
+
+
+int main()
+{
+	srand((unsigned)time(0));
+
+	// устанавливаем количество работающих потоков = 4
+	__cilkrts_set_param("nworkers", "4");
+
+	long i;
+	const long mass_size = 10;
+	int *mass_begin, *mass_end;
+	int *mass = new int[mass_size];
+
+	for (i = 0; i < mass_size; ++i)
+	{
+		mass[i] = (rand() % 25000) + 1;
+	}
+
+	mass_begin = mass;
+	mass_end = mass_begin + mass_size;
+
+	printf("Elements count = %ld\n\n", mass_size);
+	
+	ReducerMaxTest(mass, mass_size);
+	ReducerMinTest(mass, mass_size);
+
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	ParallelSort(mass_begin, mass_end);
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	duration<double> duration = (t2 - t1);
+	printf("Sort duration = %f ticks\n\n", duration.count());
+
+	ReducerMaxTest(mass, mass_size);
+	ReducerMinTest(mass, mass_size);
+
+	CompareForAndCilk_For(mass_size);
+
+	delete[]mass;
+	return 0;
+}
